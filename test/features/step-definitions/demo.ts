7493a144596a509582cc77a9dfed09d6c6c3ca84
Rehan Khan <rehan92801@hotmail.com>
2022-08-28 14:14:58 -0700

    add tags in package.json

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -19,4 +19,39 @@ Then(/^Click on the first search result$/, async function () {
 Then(/^The URL should match (.*)$/, async function (expectedUrl) {
   let url = await browser.getUrl();
   chai.expect(url).that.equal(expectedUrl);
+  browser.pause(500000)
+});
+
+/**
+ * Web Interactions
+ */
+
+Given(/^A web page is opened$/, async function () {
+  await browser.url("/inputs");
+  await browser.setTimeout({ implicit: 15000, pageLoad: 10000 });
+  await browser.maximizeWindow();
+});
+
+When(/^Perform web interactions$/, async function () {
+  /**
+   * Input box
+   * Actions:
+   * 1. Type into input box
+   * 2. Clear the field or just addvalue
+   * 3. Click and type
+   * 4. Slow typing
+   */
+  // let inputLink = await $('a[href="/inputs"]')
+  // await inputLink.click()
+
+  
+
+  let searchField = await $('input')
+  await searchField.addValue("12345")
+  await browser.pause(5000)
+
+
+
+
+
 });
